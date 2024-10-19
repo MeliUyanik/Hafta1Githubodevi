117e8275cfc86b63e77704458eab6585f386574e
Melih <melih.uyanik41@gmail.com>
2024-10-19 20:29:35 +0300

    Proje

---
+++ GedikProje1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GedikProje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2, sonuc;
+            Console.WriteLine("Birinci sayıyı giriniz: ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı girniiz: ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            sonuc = sayi1 - sayi2;
+
+            Console.WriteLine("İşlem Sonucu: " + sonuc);
+            Console.ReadKey();
+        }
+    }
+}
